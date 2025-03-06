040e6c5d1ea6422d2697d1e5ecb3bca4fce20a0f
SahiraMR <samedina91@ucatolica.edu.co>
2025-03-06 10:13:39 -0500

    nuevo1

--- src/data/data.py
+++ src/data/data.py
@@ -1,6 +1,6 @@
 class Data:
     """
-    Clase con métodos para operaciones y manipulaciones de estructuras de datos.
+    Clase con, métodos para operaciones y manipulaciones de estructuras de datos.
     Incluye implementaciones y algoritmos para arreglos, listas y otras estructuras.
     """
     
