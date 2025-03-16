628f3d974ef25cd7867f1144c6a97d00b4c5c620
SahiraMR <samedina91@ucatolica.edu.co>
2025-03-15 21:36:56 -0500

    area

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -5,23 +5,12 @@ class Geometria:
     """
     
     def area_rectangulo(self, base, altura):
-        """
-        Calcula el área de un rectángulo.
-        
-        Args:
-            base (float): Longitud de la base del rectángulo
-            altura (float): Altura del rectángulo
-            
-        Returns:
-            float: Área del rectángulo
-        """
+        return base * altura
+   
    
  
-        if base <= 0 or altura <= 0:
-            return "Error: La base y la altura deben ser mayores que cero"
-        return base * altura
-
         
+
     
     def perimetro_rectangulo(self, base, altura):
         """
